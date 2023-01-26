c467f5a821570c6c4317f26dbfcd4df609c23254
clover-park <clovertoby@naver.com>
2023-01-26 18:32:32 +0900

    create random backgroung imgs
    download images
    create random images

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 getClock(); //새로고침하자마자 1초 기다리지 않고 즉시 함수 실행
 setInterval(getClock, 1000);
