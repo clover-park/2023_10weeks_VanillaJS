77765cea8634dcb73163792bfa8fb01b34a795f9
clover-park <clovertoby@naver.com>
2023-01-29 00:27:22 +0900

    work on todos&create weather.js

--- js/todo.js
+++ js/todo.js
@@ -1,20 +1,53 @@
 const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
+const TODOS_KEY = "todos";
+
+let toDosarray = [];
 
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = todoInput.value;
   todoInput.value = "";
-  createToDo(newToDo);
+  const newToDoArray = {
+    text: newToDo,
+    id: Date.now(),
+  };
+  toDosarray.push(newToDoArray);
+  createToDo(newToDoArray);
+  saveToDo();
 }
 
 function createToDo(newToDo) {
   const list = document.createElement("li");
   const span = document.createElement("span");
+  const button = document.createElement("button");
+  button.innerText = "❌";
   list.appendChild(span);
-  span.innerText = newToDo;
+  list.appendChild(button);
+  button.addEventListener("click", removeToDo);
+  span.innerText = newToDo.text;
   todoList.appendChild(list);
 }
 
+function saveToDo() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDosarray));
+}
+
+function removeToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 todoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+//반복되는 건 변수로 fix
+//JSON : API로부터 데이터를 가져와 데이터를 좀 더 보기 쉽게 object 형식으로 만들어진 파일
+if (savedToDos !== null) {
+  const parsedToDo = JSON.parse(savedToDos);
+  toDosarray = parsedToDo;
+  // localStorage에 toDo 들이 있으면
+  // toDos에 parsedToDos를 넣어서 전에 있던 toDo들을 복원하면된다.
+  parsedToDo.forEach(createToDo); //그 array에 있는 각각의 item에 대해 실행
+}
---
+++ js/weather.js
@@ -0,0 +1,24 @@
+const API_KEY = "2918a4c5210f131fa817c206c676ce16";
+
+function onGeoOk(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  console.log("You live in", latitude, longitude);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = data.weather[0].main;
+    });
+}
+
+function onGeoError() {
+  alert("We can't find you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+//이걸 쓰면 브라우저에서 위치 좌표가 준다.
+//argument 2개 필요
